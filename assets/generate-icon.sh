69ee474c75ebacc3005f3c07af01e0be14d433f5
cho45 <cho45@lowreal.net>
2015-10-17 00:58:56 +0900

    update for windows

--- assets/generate-icon.sh
+++ assets/generate-icon.sh
@@ -16,3 +16,12 @@ mv icon_1024x1024.png icon_512x512@2x.png
 cd ..
 iconutil --convert icns --output osx/icon.icns icon.iconset
 
+convert \
+	./chemr-assets/icon_16x16.png \
+	./chemr-assets/icon_32x32.png \
+	./chemr-assets/icon_64x64.png \
+	./chemr-assets/icon_128x128.png \
+	./chemr-assets/icon_256x256.png \
+	./chemr-assets/icon_512x512.png \
+	./chemr-assets/icon_1024x1024.png \
+	win/icon.ico
